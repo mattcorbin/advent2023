43ab6b957d56520b1d2a7d746d4c61e4caef0bb7
Matt Corbin <mcorbin93@gmail.com>
2023-12-08 01:48:56 -0500

    day8 solution

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,11 +1,130 @@
+use std::collections::HashMap;
 use std::fs;
 
+#[derive(Copy, Clone, Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl From<char> for Direction {
+    fn from(value: char) -> Self {
+        match value {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => panic!("wtf"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Instructions {
+    instructions: Vec<Direction>,
+}
+
+impl From<&str> for Instructions {
+    fn from(value: &str) -> Self {
+        let mut instructions = Vec::new();
+        for c in value.chars() {
+            instructions.push(Direction::from(c));
+        }
+        Instructions { instructions }
+    }
+}
+
+fn parse_input(input: &str) -> (Instructions, HashMap<String, (String, String)>) {
+    let mut instructions = Instructions::default();
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        } else if line.contains(" = ") {
+            let mut splits = line.split(" = ");
+            let key = splits.next().unwrap().to_string();
+            let values: Vec<String> = splits
+                .next()
+                .unwrap()
+                .trim_matches(|c| c == '(' || c == ')')
+                .split(", ")
+                .map(|s| s.to_string())
+                .collect();
+            map.insert(key, (values[0].clone(), values[1].clone()));
+        } else {
+            instructions = Instructions::from(line);
+        }
+    }
+    (instructions, map)
+}
+
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let (instruction, map) = parse_input(input);
+    let end = "ZZZ".to_string();
+    let mut steps = 0;
+    let mut idx = 0;
+    let mut current = "AAA".to_string();
+    while current != end {
+        if idx == instruction.instructions.len() {
+            idx = 0;
+        }
+        let opts = map.get(&current).unwrap().clone();
+        match instruction.instructions[idx] {
+            Direction::Left => current = opts.0,
+            Direction::Right => current = opts.1,
+        }
+        steps += 1;
+        idx += 1;
+    }
+    println!("part1: {}", steps)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let (instruction, map) = parse_input(input);
+    let current: Vec<String> = map
+        .keys()
+        .filter(|k| k.ends_with("A"))
+        .map(|k| k.to_string())
+        .collect();
+    let mut cycles = Vec::new();
+    for _ in 0..current.len() {
+        cycles.push(0)
+    }
+    for (idx, val) in current.iter().enumerate() {
+        let mut inst = 0;
+        let mut first_z = 0;
+        let mut second_z = 0;
+        let mut steps = 0;
+        let mut active = val.clone();
+        loop {
+            if active.ends_with("Z") {
+                if first_z == 0 {
+                    first_z = steps;
+                } else {
+                    second_z = steps;
+                    break;
+                }
+            }
+            if inst == instruction.instructions.len() {
+                inst = 0;
+            }
+            let opts = map.get(&active).unwrap().clone();
+            match instruction.instructions[inst] {
+                Direction::Left => active = opts.0,
+                Direction::Right => active = opts.1,
+            }
+            steps += 1;
+            inst += 1;
+        }
+        cycles[idx] = second_z - first_z;
+    }
+    println!(
+        "part2: {}",
+        cycles
+            .iter()
+            .map(|x| *x / instruction.instructions.len())
+            .reduce(|a, b| a * b)
+            .unwrap()
+            * instruction.instructions.len()
+    )
 }
 
 fn main() {
