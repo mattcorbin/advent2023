ad0732ebb5bed8e473564fdf1e9268def5cd1548
Matt Corbin <mcorbin93@gmail.com>
2023-12-16 02:13:32 -0500

    update formatting for future days, fix day 14 code style

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,15 +1,32 @@
 use std::fs;
 
-fn part1(input: &str) {
-    println!("part1: {}", 0)
+fn part1(input: &str) -> usize {
+    0
 }
 
-fn part2(input: &str) {
-    println!("part2: {}", 0)
+fn part2(input: &str) -> usize {
+    0
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("input.txt should exist");
-    part1(&input);
-    part2(&input);
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        let input = fs::read_to_string("test.txt").expect("test.txt should exist");
+        assert_eq!(0, part1(&input));
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = fs::read_to_string("test.txt").expect("test.txt should exist");
+        assert_eq!(0, part2(&input));
+    }
 }
